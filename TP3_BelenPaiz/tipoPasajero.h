e7f7419553484a6e7ee8c180d5ed4631d65d7a28
Belen <belenn2206@gmail.com>
2022-06-21 18:48:15 -0300

    tp3

---
+++ TP3_BelenPaiz/tipoPasajero.h
@@ -0,0 +1,23 @@
+/*
+ * tipoPasajero.h
+ *
+ *  Created on: 20 jun 2022
+ *      Author: belen
+ */
+
+#ifndef TIPOPASAJERO_H_
+#define TIPOPASAJERO_H_
+
+typedef struct
+{
+   int id;
+   char descripcion[20];
+}eTipo;
+
+int cargarIntTipo(eTipo tipo[], int tam, char* desc);
+int cargarDescripcionTipo(eTipo tipo[], int tam, int id, char desc[]);
+int validarTipo(eTipo tipo[], int tam, int id);
+int listarTipos(eTipo tipos[], int tam);
+
+
+#endif /* TIPOPASAJERO_H_ */
